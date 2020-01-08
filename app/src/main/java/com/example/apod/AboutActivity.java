6f3f667ed753962b6599a58aa8589eb1f1df8b20
mcleod123@rambler.ru <80033Ee8>
2020-01-08 10:54:38 +0200

    - на вкладке О программе если 10 раз кликнуть на текст - появляется кот.

--- app/src/main/java/com/example/apod/AboutActivity.java
+++ app/src/main/java/com/example/apod/AboutActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +15,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
 
+    private int clickToTextCounter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        /* ---- клик 10 раз на текст------- */
+        TextView textAbout = (TextView)findViewById(R.id.about_text_content);
+        textAbout.setOnClickListener(new View.OnClickListener() {
+
+             @Override
+             public void onClick(View v) {
+                 clickToTextCounter++;
+
+                 if (clickToTextCounter == 10) {
+
+                                                 // скроем текст
+                                                 TextView textViewAbout = findViewById(R.id.about_text_content);
+                                                 textViewAbout.setVisibility(View.INVISIBLE);
+
+                                                 // скроем заголовок
+                                                 TextView textViewAbouttitle = findViewById(R.id.about_text_title);
+                                                 textViewAbouttitle.setVisibility(View.INVISIBLE);
+
+                                                 // отобразим кота
+                                                 ImageView catView = findViewById(R.id.ic_cat);
+                                                 catView.setVisibility(View.VISIBLE);
+
+                                                 // сброс значения
+                                                 clickToTextCounter = 0;
+                                             }
+
+                                         }
+
+
+
+                                     });
+        /* ------------------------------ */
 
     }
 
@@ -31,14 +69,16 @@ public class AboutActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-    /* --------------------------------- */
 
 
+    /* ----- Создание меню ------------- */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_about, menu);
         return true;
     }
+    /* --------------------------------- */
+
 
 
     /*  Выбор пунктов выпадающего меню  */
