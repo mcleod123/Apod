b008ae7f5a72150e3ef8ccfecde24c3fbcc606e3
mcleod123@rambler.ru <80033Ee8>
2020-01-11 17:02:43 +0200

    - диалог выхода из программы - аппаратная кнопка "Назад" на главной активности также показывает диалогвыхода

--- app/src/main/java/com/example/apod/AboutActivity.java
+++ app/src/main/java/com/example/apod/AboutActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +16,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
 
+    // счетчик кликов на текст О програме
+    public int clickCounter = 0;
+
+    // Анимации
+    private Animation animOne, animTwo, animThree;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // Подгружаем все анимации
+        animOne = AnimationUtils.loadAnimation(this, R.anim.animation_one);
+        animTwo = AnimationUtils.loadAnimation(this, R.anim.animation_two);
+        animThree = AnimationUtils.loadAnimation(this, R.anim.animation_three);
     }
 
 
@@ -23,6 +39,9 @@ public class AboutActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_about, menu);
         return true;
     }
+
+
+
 
 
     /*  Выбор пунктов выпадающего меню  */
@@ -47,7 +66,90 @@ public class AboutActivity extends AppCompatActivity {
     /* ------------------------------- */
 
 
+    /*  отслеживаем клики по контенту на странице "О программе"  */
+    public void OnClick(View view) {
 
+        switch(view.getId()){
+
+            // при клике на текст "О программе" будем изменять счетчик кликов и на третий клик покажем кота
+            case R.id.about_text_content:
+
+                clickCounter++;
+
+                // считаем и показываем котика
+                if(clickCounter == 3) {
+                    ShowCat();
+                }
+                break;
+
+            // при клике на картинку кота, скроем его и вернем как было
+            case R.id.about_cat_image:
+                ImageView catView = findViewById(R.id.about_cat_image);
+                AnimateCat2(catView);
+                break;
+
+            default:
+                // do nothing
+                break;
+
+
+        }
+    }
+    /* ------------------------------- */
+
+
+
+    /* показываем котика */
+    public void ShowCat() {
+
+        TextView aboutTextTitle = findViewById(R.id.about_text_title);
+        TextView aboutTextContent = findViewById(R.id.about_text_content);
+        ImageView aboutSatImage = findViewById(R.id.about_sattelite_image);
+        ImageView aboutCatImage = findViewById(R.id.about_cat_image);
+
+        // скроем все лишнее и покажем главное - кота
+        aboutTextTitle.setVisibility(View.INVISIBLE);
+        aboutTextContent.setVisibility(View.INVISIBLE);
+        aboutSatImage.setVisibility(View.INVISIBLE);
+        aboutCatImage.setVisibility(View.VISIBLE);
+
+        AnimateCat(aboutCatImage);
+        // сброс значения
+        clickCounter=0;
+    }
+    /* ------------------------------- */
+
+
+    /* скрываем котика */
+    public void HideCat() {
+
+        TextView aboutTextTitle = findViewById(R.id.about_text_title);
+        TextView aboutTextContent = findViewById(R.id.about_text_content);
+        ImageView aboutSatImage = findViewById(R.id.about_sattelite_image);
+        ImageView aboutCatImage = findViewById(R.id.about_cat_image);
+
+        // скроем кота и покажем все лишнее
+        aboutTextTitle.setVisibility(View.VISIBLE);
+        aboutTextContent.setVisibility(View.VISIBLE);
+        aboutSatImage.setVisibility(View.VISIBLE);
+        aboutCatImage.setVisibility(View.INVISIBLE);
+
+    }
+    /* ------------------------------- */
+
+
+    /* котоанимация */
+    public void AnimateCat(ImageView imageView) {
+        imageView.startAnimation(animThree);
+    }
+    /* ------------------------------ */
+
+    /* котоанимация 2 */
+    public void AnimateCat2(ImageView imageView) {
+        imageView.startAnimation(animTwo);
+        HideCat();
+    }
+    /* ------------------------------ */
 
 
 }
